9ed7d7a59a446023466ab598c22c74a3213c9354
FluffyJose <3oppbI4@gmail.com>
2019-05-22 22:43:46 +0300

    update

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 console.log('Welcome to the Brain Games!');
-import {userName} from '..';
-userName();
+//import {userName} from '..';
+//userName();
 console.log('Hi ' + userName + '!');
