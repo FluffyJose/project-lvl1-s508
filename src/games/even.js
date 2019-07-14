9c2af44dc606786619aa97927e804295e77bc7b3
Fluffy Jose <3oppbI4@gmail.com>
2019-07-14 17:36:28 +0300

    update version

--- src/games/even.js
+++ src/games/even.js
@@ -5,7 +5,7 @@ import getRandomInt from '../utils';
 const description = 'Answer "yes" if number even, otherwise answer "no".';
 const isEven = number => number % 2 === 0;
 const getQuestionAndAnswer = () => {
-  const randomInt = getRandomInt(1, 4);
+  const randomInt = getRandomInt(1, 20);
   const answer = isEven(randomInt) ? 'yes' : 'no';
   return cons(randomInt, answer);
 };
